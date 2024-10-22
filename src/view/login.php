43a98bf4fc784e63c367f16dc44aa6ce6fac32bf
Wagner F Momesso <wmomesso@gmail.com>
2024-10-21 21:58:39 -0300

    update form login

--- src/view/login.php
+++ src/view/login.php
@@ -51,22 +51,24 @@
 </video>
 
 <div class="login-container">
-    <div class="bg-white rounded-lg shadow-md p-8 max-w-sm w-full">
-        <h1 class="text-2xl font-bold text-center mb-6">Login</h1>
-        <form method="post">
-            <div class="mb-4">
-                <label for="email" class="block text-sm font-medium text-gray-700">Email:</label>
-                <input type="email" id="email" name="email" required class="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-            </div>
-            <div class="mb-6">
-                <label for="senha" class="block text-sm font-medium text-gray-700">Senha:</label>
-                <input type="password" id="senha" name="senha" required class="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
-            </div>
-            <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200">Entrar</button>
-        </form>
-        <p class="mt-4 text-sm text-center text-gray-600">
-            Não tem uma conta? <a href="#" class="text-blue-500 hover:underline">Criar conta</a>
-        </p>
+    <div class="bg-gray-900 bg-opacity-50 p-4 rounded-lg">
+        <div class="rounded-lg shadow-md p-8 max-w-sm w-full">
+            <h1 class="text-2xl font-bold text-center mb-6 text-white">WeightLoss</h1>
+            <form method="post">
+                <div class="mb-4">
+                    <label for="email" class="block text-sm font-medium text-gray-100">Email:</label>
+                    <input type="email" id="email" name="email" required class="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                </div>
+                <div class="mb-6">
+                    <label for="senha" class="block text-sm font-medium text-gray-100">Senha:</label>
+                    <input type="password" id="senha" name="senha" required class="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+                </div>
+                <button type="submit" class="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition duration-200">Entrar</button>
+            </form>
+            <p class="mt-4 text-sm text-center text-gray-400">
+                Não tem uma conta? <a href="#" class="text-gray-100 hover:underline">Criar conta</a>
+            </p>
+        </div>
     </div>
 </div>
 
